4b659e43e68d31c3a63b7d44117135cc6df39262
Yaroslav <lgalahad88@gmail.com>
2024-12-15 18:19:58 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const userNumbInput = prompt("Введіть ціле число");
+
+if (!userNumbInput?.trim()) {
+    console.log("OK! Good bye");
+}
+else {
+    const userNumb = Number(userNumbInput);
+    
+    if (isNaN(userNumb) || !Number.isInteger(userNumb)) {
+        console.log("Error! Incorrect number");
+    }
+    else if (userNumb <= 0) {
+        console.log("Задане число занадто мале, щоб квадрат будь-якого числа від 1 до 100 його не перевищував.");
+    }
+    else {
+        const validIntegers = [];
+
+        for (let i = 1; i <= 100; i++) {
+            if (i ** 2 > userNumb) break;
+            validIntegers.push(i);
+        }
+
+        console.log(`Всі цілі числа до 100, квадрат яких не перевищує ${userNumb} це: ${validIntegers.join(', ')}`);
+    }
+}
+
+
+
